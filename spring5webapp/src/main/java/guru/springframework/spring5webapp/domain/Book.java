e51d78fb90acdc064be3caf9254681f0bc1eab2b
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-12 00:47:34 +0530

    Created Publisher Entity and Done One To Many Relationship with Book

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -13,6 +13,12 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    @ManyToOne
+//    @JoinColumn(
+//            name = "",
+//            referencedColumnName = ""
+//    )
+    private Publisher publisher;
     @ManyToMany
     @JoinTable(
             name = "author_book",
@@ -22,6 +28,14 @@ public class Book {
                     name = "author_id")
             )
     private Set<Author> authors = new HashSet<>();
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
 
     public Book() {
     }
