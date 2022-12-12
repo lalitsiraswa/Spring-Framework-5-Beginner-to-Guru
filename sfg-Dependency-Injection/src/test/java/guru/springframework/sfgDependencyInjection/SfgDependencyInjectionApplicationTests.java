005793063e3ea5136e2e12142db4372d5a679238
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-13 00:30:38 +0530

    Initial Dependency Injection Project Setup

---
+++ sfg-Dependency-Injection/src/test/java/guru/springframework/sfgDependencyInjection/SfgDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgDependencyInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
