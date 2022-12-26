2992bcfddad68c93cba7b4e8c4a45b9c7ff7b7cd
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-26 22:29:30 +0530

    Spring Bean Scope Example

--- sfg-Dependency-Injection/src/main/java/guru/springframework/sfgDependencyInjection/SfgDependencyInjectionApplication.java
+++ sfg-Dependency-Injection/src/main/java/guru/springframework/sfgDependencyInjection/SfgDependencyInjectionApplication.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgDependencyInjection;
 
 import guru.springframework.sfgDependencyInjection.controllers.*;
+import guru.springframework.sfgDependencyInjection.services.PrototypeBean;
+import guru.springframework.sfgDependencyInjection.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,29 +19,41 @@ public class SfgDependencyInjectionApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(SfgDependencyInjectionApplication.class, args);
 //		MyController myController = applicationContext.getBean(MyController.class);
-		MyController myController = (MyController) applicationContext.getBean("myController");
+//		MyController myController = (MyController) applicationContext.getBean("myController");
+//
+//		PetController petController = applicationContext.getBean(PetController.class);
+//		System.out.println("------------------ The Best Pet Is ------------------");
+//		System.out.println(petController.whichPetIsTheBest());
+//
+//		System.out.println("------------------ Primary Bean ------------------");
+//		System.out.println(myController.sayHello());
+//
+//		System.out.println("------ Property Based Dependency Injection Using Spring ------");
+//		PropertyInjectedController propertyInjectedController = (PropertyInjectedController)applicationContext.getBean("propertyInjectedController");
+//		System.out.println(propertyInjectedController.getGreeting());
+//
+//		System.out.println("------ Setter Based Dependency Injection Using Spring ------");
+//		SetterInjectedController setterInjectedController = (SetterInjectedController)applicationContext.getBean("setterInjectedController");
+//		System.out.println(setterInjectedController.getGreeting());
+//
+//		System.out.println("------ Constructor Based Dependency Injection Using Spring ------");
+//		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController)applicationContext.getBean("constructorInjectedController");
+//		System.out.println(constructorInjectedController.sayGreeting());
+//
+//		System.out.println("------------------ Spring Profiles ------------------");
+//		I18nController i18nController = (I18nController)applicationContext.getBean("i18nController");
+//		System.out.println(i18nController.sayHello());
 
-		PetController petController = applicationContext.getBean(PetController.class);
-		System.out.println("------------------ The Best Pet Is ------------------");
-		System.out.println(petController.whichPetIsTheBest());
+		System.out.println("------------------ Bean Scopes ------------------");
+//		SingletonBean singletonBean1 = (SingletonBean) applicationContext.getBean("singletonBean");
+		SingletonBean singletonBean1 = applicationContext.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = applicationContext.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
 
-		System.out.println("------------------ Primary Bean ------------------");
-		System.out.println(myController.sayHello());
-
-		System.out.println("------ Property Based Dependency Injection Using Spring ------");
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController)applicationContext.getBean("propertyInjectedController");
-		System.out.println(propertyInjectedController.getGreeting());
-
-		System.out.println("------ Setter Based Dependency Injection Using Spring ------");
-		SetterInjectedController setterInjectedController = (SetterInjectedController)applicationContext.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("------ Constructor Based Dependency Injection Using Spring ------");
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController)applicationContext.getBean("constructorInjectedController");
-		System.out.println(constructorInjectedController.sayGreeting());
-
-		System.out.println("------------------ Spring Profiles ------------------");
-		I18nController i18nController = (I18nController)applicationContext.getBean("i18nController");
-		System.out.println(i18nController.sayHello());
+		PrototypeBean prototypeBean1 = applicationContext.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
 	}
 }
